0bc9f37bc5a196e432c33dfdbcf9a4bc52a98b83
deimos <salamancamanuel310@gmail.com>
2023-11-16 20:49:28 -0300

    V-For

--- frontend/src/components/ExampleFor.vue
+++ frontend/src/components/ExampleFor.vue
@@ -1,20 +1,49 @@
 <template>
-    <div>
+    <div class="d-flex flex-row bd-highlight mb-3">
+        <div class="p-2 bd-highlight">
         <ul class="list-group">
-            <li class="list-group-item" v-for="pais in paises" v-bind:key="pais">
-                {{ pais }}
+            <li class="list-group-item" v-for="pais in paises" :key="pais.code">
+                {{ pais.name }}
             </li>
         </ul>
     </div>
+    <div class="p-2 bd-highlight">
+        <ul class="list-group">
+            <li class="list-group-item" v-for="pais in paises" :key="pais.name">
+                {{ pais.code }}
+            </li>
+        </ul>
+    </div>
+</div>
 </template>
 
 <script setup lang="ts">
 import { Ref, ref } from 'vue';
-let paises: Ref<Array<string>> = ref([
-    'Argentina',
-    'España',
-    'UK',
-    'Portugal'
+interface IPaises{
+    code: string,
+    name: string
+}
+let paises:Ref<Array<IPaises>> = ref([
+    {
+        code: 'es',
+        name: 'España'
+    },
+    {
+        code: 'Fr',
+        name: 'Francia'
+    },
+    {
+        code: 'Ar',
+        name: 'Argentina'
+    },
+    {
+        code: 'EEUU',
+        name: 'Estados Unidos'
+    },
+    {
+        code: 'MX',
+        name: 'Mexico'
+    },
 ])
 </script>
 
