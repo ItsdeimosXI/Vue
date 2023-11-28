52b80de085f672a2ac6b4bc21799468f8179e820
deimos <salamancamanuel310@gmail.com>
2023-11-28 20:41:39 -0300

    Post con setup

--- frontend/src/components/PostDetail.vue
+++ frontend/src/components/PostDetail.vue
@@ -1,40 +1,29 @@
 <template>
     <div class="p-2 bd-highlight">
-        <h3>{{ props.titulo}}</h3>
-        <p>{{ props.contenido}}</p>
+        <h3>{{ props.titulo }}</h3>
+        <p>{{ props.contenido }}</p>
         <img src="../assets/XDD.png" alt="XD">
         <input type="text" v-model="mensaje">
-        <button class="btn btn.primary" @click="Click" >Di hola</button>
+        <button class="btn btn.primary" @click="Click">Di hola</button>
     </div>
 </template>
 
-<script lang="ts">
-import { Ref, defineComponent, ref } from 'vue';
+<script setup lang="ts">
+import { defineProps, defineEmits, Ref, ref } from 'vue';
 
-export default defineComponent({
-    name: 'PostDetail',
-    props: {
-        titulo:{
-            type: String,
-            required: true
-        },
-        contenido:{
-            type: String,
-            required: false,
-            default: 'Este post no tiene contenido'
-        }
-    },
-    emits: ["Dihola"],
-    setup(props, {emit} ) {
-        const Click = () => {
-           emit( "Dihola", mensaje.value)
-        }
-        let mensaje:Ref<string> = ref("")
-        return {props, Click, mensaje}
-    },
-})
+const props = defineProps(
+    { titulo: String,
+     contenido: String}
+)
+
+const emit = defineEmits(
+    ['Dihola']
+)
+const Click = () => {
+    emit("Dihola", mensaje.value)
+}
+let mensaje: Ref<string> = ref("")
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
